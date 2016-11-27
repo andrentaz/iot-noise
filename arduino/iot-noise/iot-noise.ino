4108b1ed3484b34bed733ca5a65f6a223219b901
Andre Filliettaz <andrentaz@gmail.com>
2016-11-26 23:31:19 -0200

    Change time recording and collecting process

--- arduino/iot-noise/iot-noise.ino
+++ arduino/iot-noise/iot-noise.ino
@@ -9,8 +9,13 @@
  * SPL meter - Davide Girone: http://davidegironi.blogspot.com.br/2014/02/a-simple-sound-pressure-level-meter-spl.html#.WCxnlHUrK03
  * ESP8266   - FilipeFlop: http://blog.filipeflop.com/wireless/esp8266-arduino-tutorial.html 
  */
+//#define LCD
+
 #include <SoftwareSerial.h>
+
+#ifdef LCD
 #include <LiquidCrystal.h>
+#endif
 
 #include "adc.h"
 #include "audioget.h"
@@ -22,40 +27,50 @@
 #define PIN_RST 10  // RST pin
 
 // Data
-#define ID "4"
+#define ID "5"
 
 // Debug option
 #define DEBUG true
 
 // Constants
-const int WINDOW = 50;
-const int MAX = 50;
+const int MAX = 20;
+#ifdef LCD
 const int ncols = 16;
 const int nrows = 2;
+#endif
 
 // Network
-const String WLAN_SSID = "filliettaz";
-const String WLAN_PASS = "20081991";
-const String SERVER_ADDR = "192.168.0.6";
-const long SERVER_PORT = 8080;
+#define WLAN_SSID "filliettaz"
+#define WLAN_PASS "20081991"
+#define SERVER_ADDR "192.168.0.6"
+#define SERVER_PORT 8080
 
 // Network
 SoftwareSerial Wifi(PIN_RX,PIN_TX);
 
 // LCD
+#ifdef LCD
 LiquidCrystal lcd(9,8,7,6,5,4);
+#endif
 
 void setup() {
     // begin the serials connections
     Serial.begin(9600);
     Wifi.begin(9600);
+
+    #ifdef LCD
     lcdSetup();
+    #endif
 
     // Pin modes
     pinMode(PIN_RST, OUTPUT);
 
     while (!Serial);
+
+    #ifdef LCD
     lcdPrint("Connecting...", 0, 2000, true);
+    #endif
+
     resetWifi();
     connectWifi();
     Serial.println("End setup");
@@ -64,7 +79,9 @@ void setup() {
 void loop() {
     // count and average spl
     int count = 1;
-    double avgSpl = 0.0;
+    int ns = 1;
+    long spend = 0;
+    double avgspl = 0.0;
 
     // get current reference voltage
     double refv = adc_getrealvref();
@@ -72,10 +89,14 @@ void loop() {
     int16_t audiorms = 0;
     int16_t audiospl = 0;
 
+    String tmvlist = "[";
+
     // input channel is setted to 0
     audioget_init();
 
     while(true) {
+        // mark time
+        long start = millis();
         // get samples
         audioget_getsamples();
         // compute fft
@@ -94,22 +115,41 @@ void loop() {
         }
 
         audiospl = audioget_getspl(rmsvolts, AUDIOGET_VOLTREF, AUDIOGET_DBREF);
+        spend += millis() - start;
 
-        // send data to the server
-        if (count > MAX+1) {
-            sendScreen();
-            lcdPrint("Sending...", 0, 2000, true);
-            sendToServer(avgSpl);
-            count = 0;
-            avgSpl = 0;
+        if (spend > 200) {
+            tmvlist.concat(spend);
+            tmvlist.concat(",");
+            tmvlist.concat(avgspl);
+
+            // send data to the server
+            if (count > MAX+1) {
+                #ifdef LCD
+                sendScreen();
+                lcdPrint("Sending...", 0, 2000, true);
+                #endif
+
+                tmvlist.concat( "]");
+                sendToServer(tmvlist);
+                count = 0;
+                tmvlist = "[";
+            } else {
+                #ifdef LCD
+                // write in the lcd
+                printScreen(audiorms, floor(audiospl));
+                #endif
+
+                // save the average
+                tmvlist.concat(",");
+            }
+            count++;
+            spend = 0;
+            ns = 0;
+            avgspl = 0.0;
         } else {
-            // write in the lcd
-            printScreen(audiospl, floor(audiospl));
-
-            // save the average
-            avgSpl += abs(avgSpl - audiospl)/count;
+            avgspl += abs(avgspl - audiospl)/ns;
         }
-        count++;
+        ns++;
     }
 }
 
@@ -122,98 +162,120 @@ void resetWifi() {
     delay(1000);
     digitalWrite(PIN_RST, HIGH);
     delay(500);
-    ATCommand("AT+CWMODE=1\r\n", "CWMODE=1", 3000, DEBUG);
-    ATCommand("AT+CIPMUX=0\r\n", "CIPMUX=0", 3000, DEBUG);
+    ATCommand("AT+CWMODE=1\r\n", "CWMODE=1", 1000, DEBUG);
+    ATCommand("AT+CIPMUX=0\r\n", "CIPMUX=0", 1000, DEBUG);
 }
 
 void connectWifi() {
     String cmd = "AT+CWJAP=\"";
-    cmd += WLAN_SSID;   cmd += "\",\"";
-    cmd += WLAN_PASS;   cmd += "\"\r\n";
+    cmd.concat(WLAN_SSID);   cmd.concat("\",\"");
+    cmd.concat(WLAN_PASS);   cmd.concat("\"\r\n");
     ATCommand(cmd, "CWJAP", 10000, DEBUG);
 }
 
-void sendToServer(float value) {
-    String valueStr = "";
-    String vref;
-    String dbref;
-    String json = "";
-
-    valueStr += value;
-    vref += AUDIOGET_VOLTREF;
-    dbref += AUDIOGET_DBREF;
-    
-    lcdPrint("SPL: " + valueStr, 0, 2000, true);
-
-    // Start the connection
-    String cmd = "AT+CIPSTART=\"TCP\",\"";
-    cmd += SERVER_ADDR; cmd += "\",";
-    cmd += SERVER_PORT; cmd += "\r\n";
+void sendToServer(String value) {
+    #ifdef LCD
     lcdPrint("CIPSTART", SERVER_ADDR+":"+SERVER_PORT,1000,false);
-    Wifi.print(cmd);
+    #endif
+
+    Wifi.print("AT+CIPSTART=\"TCP\",\"");
+    Wifi.print(SERVER_ADDR); Wifi.print("\",");
+    Wifi.print(SERVER_PORT); Wifi.print("\r\n");
     delay(10000);
 
     if (Wifi.find("OK")) {
+        #ifdef LCD
         lcdPrint("TCP conn OK!", 0, 2000, true);
+        #endif
         
-        // prepare command to send
-        cmd = "AT+CIPSEND=";
+        int len = 0;
 
         // prepare JSON data
-        json = "{\"id\":\"";
-        json += ID;
-        json += "\",\"vref\":\"";
-        json += vref;
-        json += "\",\"dbref\":\"";
-        json += dbref;
-        json += "\",\"spl\":\"";
-        json += valueStr;
-        json += "\"}";
+        len += strlen("{\"id\":\"");
+        len += strlen(ID);
+        len += strlen("\",\"vref\":\"");
+        len += strlen(String(AUDIOGET_VOLTREF).c_str());
+        len += strlen("\",\"dbref\":\"");
+        len += strlen(String(AUDIOGET_DBREF).c_str());
+        len += strlen("\",\"tmvlist\":");
+        len += value.length();
+        len += strlen("}");
         
-        int len = json.length();
+        int sendsize = 0;
 
         // prepare POST String
-        String postRequest = "POST /csv/upload HTTP/1.1\r\n";
-        postRequest += "Host: " + SERVER_ADDR;
-        postRequest += "\r\n";
-        postRequest += "Content-Length: ";
-        postRequest += len;
-        postRequest += "\r\n";
-        postRequest += "Content-Type: application/json\r\n\r\n";
+        sendsize += strlen("POST /csv/upload HTTP/1.1\r\n");
+        sendsize += strlen("Host: "); 
+        sendsize += strlen(SERVER_ADDR);
+        sendsize += strlen("\r\n");
+        sendsize += strlen("Content-Length: ");
+        sendsize += strlen(String(len).c_str());
+        sendsize += strlen("\r\n");
+        sendsize += strlen("Content-Type: application/json\r\n\r\n");
+        sendsize += strlen("{\"id\":\"");
+        sendsize += strlen(ID);
+        sendsize += strlen("\",\"vref\":\"");
+        sendsize += strlen(String(AUDIOGET_VOLTREF).c_str());
+        sendsize += strlen("\",\"dbref\":\"");
+        sendsize += strlen(String(AUDIOGET_DBREF).c_str());
+        sendsize += strlen("\",\"tmvlist\":");
+        sendsize += value.length();
+        sendsize += strlen("}");
+        sendsize += strlen("\r\n\r\n");
 
-        postRequest += json;
-        postRequest += "\r\n\r\n";
-
-        cmd += strlen(postRequest.c_str());
-        cmd += "\r\n";
-
-        // Send the command
-        lcdPrint("CIPSEND", strlen(postRequest.c_str()), 1000, false);
-        Wifi.print(cmd);
-        delay(5000);
+        Wifi.print("AT+CIPSEND=");
+        Wifi.print(sendsize);
+        Wifi.print("\r\n");
+        delay(10000);
 
         // Server is waiting for data
         if (Wifi.find(">")) {
             flush(Wifi);
             // Start the sending process
+            #ifdef LCD
             lcdPrint("Sending...", millis(), 2000, false);
+            #endif
 
             // Send data
-            Wifi.print(postRequest);
+            Wifi.print("POST /csv/upload HTTP/1.1\r\n");
+            Wifi.print("Host: "); 
+            Wifi.print(SERVER_ADDR);
+            Wifi.print("\r\n");
+            Wifi.print("Content-Length: ");
+            Wifi.print(len);
+            Wifi.print("\r\n");
+            Wifi.print("Content-Type: application/json\r\n\r\n");
+            Wifi.print("{\"id\":\"");
+            Wifi.print(ID);
+            Wifi.print("\",\"vref\":\"");
+            Wifi.print(AUDIOGET_VOLTREF);
+            Wifi.print("\",\"dbref\":\"");
+            Wifi.print(AUDIOGET_DBREF);
+            Wifi.print("\",\"tmvlist\":");
+            Wifi.print(value);
+            Wifi.print("}");            
+            Wifi.print("\r\n\r\n");
             delay(20000);
 
             // Log the ending
+            #ifdef LCD
             lcdPrint("Ending...", millis(), 2000, false);
-
-            delay(100);
+            #endif
         } else {
+            #ifdef LCD
             lcdPrint("Send Problems", 0, 2000, true);
+            #endif
+
             flush(Wifi);
         }
-        
+
+        flush(Wifi);
         ATCommand("AT+CIPCLOSE\r\n", "CIPCLOSE", 5000, DEBUG);
     } else {
+        #ifdef LCD
         lcdPrint("Conn Problems", 0, 2000, false);
+        #endif
+
         resetWifi();
         connectWifi();
     }
@@ -221,7 +283,10 @@ void sendToServer(float value) {
 
 String ATCommand(String command, String type, const int timeout, bool debug) {
     // Send AT command to the module
+    #ifdef LCD
     lcdPrint("Sending command!", type, 0, false);
+    #endif
+
     String response = "";
     flush(Wifi);
     Wifi.print(command.c_str());
@@ -230,7 +295,7 @@ String ATCommand(String command, String type, const int timeout, bool debug) {
         while (Wifi.available()) {
             // The esp has data so display its output to the serial window
             char c = Wifi.read();    // read the next char
-            response += c;
+            response.concat(c);
         }
     }
     if (debug) {
@@ -239,6 +304,7 @@ String ATCommand(String command, String type, const int timeout, bool debug) {
     return response;
 }
 
+#ifdef LCD
 /* -------------------------------------------------------------------------- */
 /* LCD Screen API */
 /* -------------------------------------------------------------------------- */
@@ -255,7 +321,7 @@ void lcdSetup()
 void printScreen(float avg, int spl) {
     // Print first line
     lcd.setCursor(0,0);
-    lcd.print("Avg Amp: ");
+    lcd.print("RMS: ");
     lcd.setCursor(9, 0);
     for (int i=9; i<ncols; i++) {
         lcd.print(" ");
@@ -283,6 +349,7 @@ void sendScreen() {
     lcd.clear();
 }
 
+
 void lcdPrint(String str, int line, int timeout, bool clean) {
     lcd.clear();
     lcd.setCursor(0,line);
@@ -306,6 +373,7 @@ void lcdPrint(String fst, String snd, int timeout, bool clean) {
         lcd.clear();
     }
 }
+#endif
 
 void flush(SoftwareSerial serial) {
     while (serial.available() > 0) {
--- server/server.js
+++ server/server.js
@@ -14,21 +14,37 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // callback for post to upload
-function callback_csv (req, res) {
+function cb_postcsv (req, res) {
     // get the timestamp
-    var timestmp = moment();
+    var timestamp = moment();
 
     // get the attibutes 
     var id = req.body.id;
     var vref = req.body.vref;
     var dbref = req.body.dbref;
-    var spl = req.body.spl;
-    var tmp = timestmp.format().split('T').join(' ');
+    var tmvlist = req.body.tmvlist;
 
+    // log it
+    console.log('POST Request for file: ' + id + '.csv');
+    console.log('Request for saving: ' + tmvlist.length/2 + ' records');
+    console.log('Received time: ' + timestamp.format());
 
     // create the line
-    var content = [id, tmp, vref, dbref, spl].toString() + "\n";
+    var content = '';
     var filepath = 'csv/upload/' + id + ".csv";
+
+    var stime = timestamp;
+    for (var i = tmvlist.length-1; i > 0; i--) {
+        // get the variables
+        var valdb = tmvlist[i];
+        var msec = tmvlist[i-1];
+        stime = stime.subtract(msec, 'ms');
+        var ftt = stime.format('YYYY/MM/dd HH:mm:ss.SSSS');
+
+        // print the line in content
+        content = [id,vref,dbref,ftt,msec,valdb].toString() + '\n' + content;
+        i--;
+    }
 
     // write the file
     fs.appendFile(filepath, content, function (err) {
@@ -44,20 +60,25 @@ function callback_csv (req, res) {
         // send response to the client
         res.send (JSON.stringify({
             file: req.body.id,
+            time: 'Received at: ' + timestamp.format().split('T').join(' '),
             status: "OK"
         }));
     });
 }
 
-// initially, for debug purposes 
-function callback_get (req, res) {
-    console.log("Got a request");
-    res.send("Received\n");
+// Send file callback
+function cb_getcsv(req, res) {
+    // get the file Id to be sent
+    var id = req.query.fileId;
+    console.log("GET Request for file: " + id + ".csv");
+    var filepath = './csv/upload/' + id + '.csv';
+    res.sendFile(filepath, { root: __dirname });
+    console.log("File sent");
 }
 
 // routing
-app.post('/csv/upload', callback_csv);
-app.get('/', callback_get);
+app.post('/csv/upload', cb_postcsv);
+app.get('/csv/upload', cb_getcsv);
 
 // initialize Server
 app.listen(8080, function () {
